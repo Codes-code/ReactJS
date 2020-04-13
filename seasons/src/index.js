11350f1cb8cde0d6a3b87e4a6ffc5b7fe7c247e5
Ayush Jindal <ayushjindal1997@hotmail.com>
2020-04-14 02:56:26 +0530

    .

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,28 @@
+//To understand the differences between Class Components and Functional Components
+/* if the user's location is northern hemisphere and between october and march OR south 
+and between match and october THEN chilly but if northern and between march and october 
+OR south and between october and march THEN hit the BEACH */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+
+const App = () => { //functional component
+
+    window.navigator.geolocation.getCurrentPosition(
+        position=>console.log(position),
+        err=>console.log(err)
+    );
+
+    return(
+    <div className="ui icon header">
+        <h3>Setting Hori</h3>
+    </div>
+    );
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, 
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
