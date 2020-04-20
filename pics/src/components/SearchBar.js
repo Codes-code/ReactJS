8d4a7787051adc1a1edaced34e39c6a7609ea8bc
Ayush Jindal <ayushjindal1997@hotmail.com>
2020-04-20 16:45:34 +0530

    j

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-    state={term:''};
+    state={term:'search cars..'};
     
     onInputClicked(event) {
         console.log("input clicked--"+ event.target.value); 
-        //console.log(this.state.term);
+        console.log(this.state.term);
     }
 
     /*onInputChanged(e) {
@@ -14,6 +14,14 @@ class SearchBar extends React.Component {
         //console.log(this.state.term);
     }*/
 
+    onFormSubmit(e) {
+        e.preventDefault();
+        console.log('form submitted--'+ this.state.term)
+
+    }
+
+
+    //THIS refers to the object to the left of the function call.
 
 
 
@@ -25,19 +33,20 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className='SearchBar ui segment'>
-                <form className='ui form'>
+                <form  onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
                         <i className='car icon' onClick={(e)=>this.setState({term: 'Alfa Romeo Guilia Quadrifoglio'})}></i>
                         <label>Image Search</label>
                         <input
                             type="text"
                             onClick={this.onInputClicked} 
-                            value={this.state.term} //provides the pre-render value to the input text.
-                            //onChange={this.onInputChanged}
-                            onChange={(e)=>this.setState({term: e.target.value})}//takes the value, puts it in state, that causes the render() to be called, which erases the previous input text, so now we provide it with the value before it ahd re-rendered. 
+                            value={this.state.term}
+                            onChange={(e)=>this.setState({term: e.target.value})}
+                            //takes the value, puts it in state, that causes the render() to be called, which erases the previous input text, so now we provide it with the term through the 'value=' before it had re-rendered. we're doing unnecessary work, just to make sure 'state' and 'react' have all the power. 
+                            //for eg. if we want Capital text to appear, no matter what the user types. then we can do that. or if we want starred items to appear no matter what the user types. 
                         >
                         </input>
-                        <div>{this.state.term}</div>
+                        {/*<div>{this.state.term}</div>*/}
                     </div>
                 </form>
             </div>
