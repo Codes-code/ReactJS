27884144fc8187dc0a528aaae02062444758fef3
Ayush Jindal <ayushjindal1997@hotmail.com>
2020-04-21 15:18:24 +0530

    image api called.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends React.Component {
 
-    state={ n:null, counter:0};
+    state={ n:null, counter:0, images:null};
+
+    async onSearchSubmit(searchterm) {
+
+        console.log("form submitted-- "+ searchterm);
+
+        const response = await axios.get( 
+            'https://api.unsplash.com/search/photos',
+            {
+                params: {query: searchterm},
+                headers: {Authorization : 'Client-ID euvETru-dyZew23SFLrejHKJLSXynaR0brs4fDw68Ms'}
+            }
+        )
+
+        console.log(response.data.results);
+    }
 
     render() {
         return(
             <div>
                 <h1>Cars App</h1>
-                <div className='SearchBar ui container' style={{marginTop:'10px'}}><SearchBar/></div>
-                <div className='ImageList'><ImageList/></div>
+                <div className='SearchBar ui container' style={{marginTop:'10px'}}><SearchBar whenSubmit= {this.onSearchSubmit} /></div>
+                <div className='ImageList'><ImageList /></div>
             </div>
         );
     }
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-    state={term:'search cars..'};
+    state={term:'car'};
     
-    onInputClicked(event) {
-        console.log("input clicked--"+ event.target.value); 
-        console.log(this.state.term);
+    onInputClicked = (event) => {
+        console.log("input clicked-- "+ event.target.value); 
     }
 
-    /*onInputChanged(e) {
-        this.setState({term: e.target.value});
-        //console.log(this.state.term);
-    }*/
-
-    onFormSubmit(e) {
+    onFormSubmit = (e) => {
         e.preventDefault();
-        console.log('form submitted--'+ this.state.term)
-
+        {this.props.whenSubmit(this.state.term)};//props of the class 'searchbar' , we don't pass it in brackets in the declaration of class. 
     }
 
 
     //THIS refers to the object to the left of the function call.
+    /* Ways of solving 'this' problem 1-- make the callback function,
+     an arrow function. that makes it refer to the main class. 
+     2-- make a constructor and bind the function with the current class
+      using this.example=this.drive.bind(this) 3-- is to invoke an arrow function, 
+      to call the main function itself. so ina sense using 2 function calls. 
+      like  onSubmit={(e) => this.onFormSubmit(e)  , this method is also using 
+        the arrow function route.  */
 
 
 
@@ -35,7 +35,7 @@ class SearchBar extends React.Component {
             <div className='SearchBar ui segment'>
                 <form  onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
-                        <i className='car icon' onClick={(e)=>this.setState({term: 'Alfa Romeo Guilia Quadrifoglio'})}></i>
+                        <i className='car icon' onClick={(e)=>this.setState({term: 'Alfa Romeo'})}></i>
                         <label>Image Search</label>
                         <input
                             type="text"
@@ -46,7 +46,6 @@ class SearchBar extends React.Component {
                             //for eg. if we want Capital text to appear, no matter what the user types. then we can do that. or if we want starred items to appear no matter what the user types. 
                         >
                         </input>
-                        {/*<div>{this.state.term}</div>*/}
                     </div>
                 </form>
             </div>
