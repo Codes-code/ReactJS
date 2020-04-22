1c9aa3199b9e7341251acbc03080402b47c629a9
Ayush Jindal <ayushjindal1997@hotmail.com>
2020-04-22 08:01:59 +0530

    Notes added. constructor, controllable items, 'this' issue.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,7 +5,21 @@ import ImageList from './ImageList';
 
 class App extends React.Component {
 
-    state={ n:null, counter:0, images:null};
+    constructor(props){
+
+        super(props);
+        this.onSearchSubmit=this.onSearchSubmit.bind(this);
+        /* Using this binding to reconfigure our 'this' because here our this refers to onSubmit. 
+        - down where i do this.setState({}), that 'this' is unable to refer our app component because
+        it refers to it's callback, so go to where the onSearchSubmit is being called. from where searchterm
+        is coming. it's coming from searchbar.js , there it's being called from onFormSubmit, as whensubmit. 
+        now, in there the item to the left of the function call whensubmit(), is 'props.', hence this state is refering to
+        the object 'props' of onFormSubmit. which is coming from this.onformsubmit through OnSubmit property 
+        of the Form div. So, hence the whole state was refering to the OnSubmit. and now we've forced it to bind
+        to our App component, and not the OnSubmit.  */ 
+        
+        this.state={ n:null, counter:0, images:[]};
+    }
 
     async onSearchSubmit(searchterm) {
 
@@ -17,9 +31,11 @@ class App extends React.Component {
                 params: {query: searchterm},
                 headers: {Authorization : 'Client-ID euvETru-dyZew23SFLrejHKJLSXynaR0brs4fDw68Ms'}
             }
-        )
+        );
 
-        console.log(response.data.results);
+        this.setState({images: response.data.results});
+        console.log(this.state.images);
+
     }
 
     render() {
