06fe049ed87d3b41d3d8c2758bb50ef87fdc3c0a
Ayush Jindal <ayushjindal1997@hotmail.com>
2020-04-26 17:27:18 +0530

    a

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import SI from './Seperateimages';
+import SI from './Seperateimages';//din't work, try to make it work. 
+import '../css/ImageList.css';
 class ImageList extends React.Component {
 
     /*
@@ -15,26 +16,19 @@ class ImageList extends React.Component {
         console.log('seperateimage called');
 
         return  this.props.images.map((image) =>  {
-                    return <div key={image.id}><img alt={image.description} src={image.urls.thumb}></img></div>
+                    return  <div key={image.id}>
+                                <img alt={image.description} src={image.urls.thumb}></img>
+                            </div>
             });
     }
     //dame dame, the 
     render() {
 
         return (
-            <div className='ImageList ui segment' style={{marginTop:'30px'}}>
-                <h5>Image List</h5>
-                <div>No. of Images Found: {this.props.images.length}</div>
-
-                {/*<div>
-                    <SI 
-                        images={this.props.images} 
-                        size='thumb' 
-                    /> 
-                </div>*/}
-
-                <div style={{display: grid}}>{this.seperateimage()}</div>
-
+            <div className='image-list ui segment' style={{marginTop:'30px'}}>
+                <div className='image-list-images'>
+                    {this.seperateimage()}
+                </div>
             </div>
         );
     }
